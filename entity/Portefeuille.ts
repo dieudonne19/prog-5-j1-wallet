af993e300902364efabea027dfe224acdb44d4d4
dieudonne19 <hei.dieudonne.19@gmail.com>
2025-11-19 09:42:02 +0300

    feat: set up project

---
+++ entity/Portefeuille.ts
@@ -0,0 +1,12 @@
+export class Portefeuille {
+  private couleur: string;
+  private porteCarte: string;
+  private poids: string;
+  private marque: string;
+
+  // getMoney
+  // addMoney
+  // checkMoney
+  // addCard
+  // lost
+}
